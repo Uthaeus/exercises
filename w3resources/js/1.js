c0660fbc0b4a6725c84e78ef178299d3a686db52
Uthaeus <romanlavery@gmail.com>
2020-08-14 14:39:30 -0600

    1

---
+++ w3resources/js/1.js
@@ -0,0 +1,6 @@
+// Write a JavaScript program to compare two objects to determine if the first one contains equivalent property values to the second one
+
+
+function comp(a, b) {
+  return a == b;
+}
