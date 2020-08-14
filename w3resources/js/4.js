df80b4a68135abc8ef40f986728ab17ad6afe680
Uthaeus <romanlavery@gmail.com>
2020-08-14 15:19:46 -0600

    find the area of a triangle

---
+++ w3resources/js/4.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
+// A = hb/2
+
+
+const area = (a, b, c) => {
+  return (a * b) / 2
+}
+
