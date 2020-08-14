abb632817b755bc5ecde504f97b0fe1f9ce69331
Uthaeus <romanlavery@gmail.com>
2020-08-14 14:58:04 -0600

    display current day and time

--- w3resources/js/1.js
+++ w3resources/js/1.js
@@ -1,6 +1,28 @@
-// Write a JavaScript program to compare two objects to determine if the first one contains equivalent property values to the second one
+// 1. Write a JavaScript program to display the current day and time in the following format.  Go to the editor
+// Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
 
 
-function comp(a, b) {
-  return a == b;
+function getCurrentTime() {
+  let today = new Date();
+  let day = today.getUTCDay();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let seconds = today.getSeconds();
+  let sig;
+  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  if (hour > 11) { 
+    sig = 'PM'; 
+  } else {
+    sig = 'AM';
+  }
+
+  if (hour > 12) {
+    hour = hour - 12;
+  }
+
+  return  `Today is : ${days[day]}.\nCurrent time is : ${hour} ${sig} : ${minute} : ${seconds}`
 }
+
+console.log(getCurrentTime());
