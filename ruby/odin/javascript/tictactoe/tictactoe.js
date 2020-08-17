a6741ac79adf142535e3ef15f5dc05c334ffa008
Uthaeus <romanlavery@gmail.com>
2020-08-17 14:40:49 -0600

    building intro and gameboard

---
+++ ruby/odin/javascript/tictactoe/tictactoe.js
@@ -0,0 +1,25 @@
+
+const intro = () => {
+    let el = document.getElementById('introBoard');
+    let tag = document.createElement('p');
+    let text = document.createTextNode("Welcome to tictactoe");
+
+    tag.appendChild(text);
+    el.appendChild(tag);
+}
+
+const newBoard = () => {
+
+    for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 3; y++) {
+            createTile(x, y);
+        }
+    }
+}
+
+
+// const createTile = (x, y, value = '') {
+    
+// }
+
+intro();
